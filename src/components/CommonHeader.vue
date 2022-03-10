664c5d0f5096b00d5c788a6bf1f549c02e737dcb
jin yi <jlee@cookapps.com>
2022-03-10 13:57:37 +0900

    chore: 링크 추가

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="text-center" style="padding: 20px 0 40px; color: #000;">
     <h3 style="margin-bottom: 8px;" class="highlight"><b>FLEX is EASY.</b></h3>
-    <h4 style="margin-bottom: 12px;">Built by Jin Yi 🥾</h4>
+    <h4 style="margin-bottom: 12px;">Built by <a style="color: #000; background: none; text-decoration: underline;" href="https://www.notion.so/jindev/h1-Jin-Yi-FE-Developer-h1-df545fd0a6de4b0e8a0ce1afb96b68fb" target="_blank">Jin Yi 🥾</a></h4>
     <a style="color: #FFF; background: none; text-decoration: underline;" href="https://github.com/viva-jinyi/FLEXisEASY" target="_blank">github link</a>
   </header>
 </template>

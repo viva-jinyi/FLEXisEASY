b1799317796eb0f5c13de3c1a96cf95fb4d6c14a
jin yi <jlee@cookapps.com>
2022-03-10 13:01:15 +0900

    chore: github link 수정

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -2,7 +2,7 @@
   <header class="text-center" style="padding: 20px 0 40px; color: #000;">
     <h3 style="margin-bottom: 8px;" class="highlight"><b>FLEX is EASY.</b></h3>
     <h4 style="margin-bottom: 12px;">Built by Jin Yi 🥾</h4>
-    <a style="color: #222;" href="https://www.notion.so/yesiamjin/h1-Jin-Yi-Jr-Developer-h1-df545fd0a6de4b0e8a0ce1afb96b68fb" target="_blank">github link</a>
+    <a style="color: #FFF; background: none; text-decoration: underline;" href="https://github.com/viva-jinyi/FLEXisEASY" target="_blank">github link</a>
   </header>
 </template>
 
